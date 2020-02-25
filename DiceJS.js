a61a032e5d538425270d4449cbbfc1ea7f468e17
Abraham Sanchez <sanchez95san@gmail.com>
2020-02-25 17:00:41 -0600

    Hardcoded game to occur in javascript, will begin moving into html facing properties

--- DiceJS.js
+++ DiceJS.js
@@ -17,55 +17,83 @@ function PopulatePlayers(){
 }
 
 function RoundOneThruThree(players){
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < players.length; i++) {
     players[i].score = RollDiceSet();
     
     
 }
     console.log(players);
     players.sort(function(a,b){return a.score - b.score});
-    players.reverse();
-    players.pop();
-    players.pop();
+    
+    players.splice(0,2);
     console.log(players);
 
     
 }
 
 function RoundFourAndFive(players){
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < players.length; i++) {
         players[i].score = RollDiceSet();
         
         
     }
         console.log(players);
         players.sort(function(a,b){return a.score - b.score});
-        players.reverse();
-        players.pop();
+       
+        players.shift();
         console.log(players);
 }
  function FinalRound(players){
+    var dTwentyRollsForPlayerOne = [];
+    for (let i = 0; i < 4; i++) {
+        dTwentyRollsForPlayerOne.push(randomDiceTotal(20));
+        
+    }
+    players[0].score = dTwentyRollsForPlayerOne[(randomDiceTotal(4))-1]
+    
+    var dTwentyRollsForPlayerTwo = [];
+    for (let i = 0; i < 4; i++) {
+        dTwentyRollsForPlayerTwo.push(randomDiceTotal(20));
+        
+    }
+    players[1].score = dTwentyRollsForPlayerOne[(randomDiceTotal(4))-1]
+    if (players[0].score > players[1].score) {
+        console.log(players[0].name + " wins!");
+    }
+    else if (players[0].score == players[1].score)
+    {
+        console.log("Tie, repeating final round");
+        FinalRound(players);
+    }
+    else
+    {
+        console.log(players[1].name + " wins!");
+    }
+}
 
-
- } 
+  
  function RollDiceSet(){
     var result = 0;
      for (let i = 0; i < DiceTotals.length; i++) {
-         result += DiceSum(DiceTotals[i]);
+         result += randomDiceTotal(DiceTotals[i]);
          
      };
      
      return result;
  }
- function DiceShootOut(){
-
- }
- function DiceSum(Limit){
+ function randomDiceTotal(Limit){
      return Math.floor((Math.random() * Limit)+1);
  }
 
 
 PopulatePlayers();
-console.log(players);
+
 RoundOneThruThree(players);
-console.log(players);
+RoundOneThruThree(players);
+RoundOneThruThree(players);
+RoundFourAndFive(players);
+RoundFourAndFive(players);
+FinalRound(players);
+
+
+
